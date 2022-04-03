f697c7d0b94547551f649726cb6ea5c3b68b7ec2
valeria <valeria.ropot2@gmail.com>
2022-04-03 14:43:44 +0300

    edit QuestionsController and create AnswersController

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,19 @@
 class QuestionsController < ApplicationController
+  before_action :load_question, only: [:show, :edit]
+
+  def index
+    @questions = Question.all
+  end
+
+  def show
+  end
 
   def new
     @question = Question.new
   end
 
+  def edit
+  end
 
   def create
     @question = Question.new(question_params)
@@ -17,6 +27,9 @@ class QuestionsController < ApplicationController
 
   private
 
+  def load_question
+    @question = Question.find(params[:id])
+  end
 
   def question_params
     params.require(:question).permit(:title, :body)
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -3,6 +3,33 @@ require 'rails_helper'
 RSpec.describe QuestionsController, type: :controller do
   let(:question) {create(:question)}
 
+  describe 'GET #index' do
+    let(:questions) {create_list(:question, 3)}
+
+    before { get :index }
+
+    it 'populates an array of all questions' do
+      expect(assigns(:questions)).to match_array(questions)
+    end
+
+    it 'renders index view' do
+      expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+
+    before { get :show, params: {id: question} }
+
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template :show
+    end
+  end
+
   describe 'GET #new' do
     before { get :new }
 
@@ -12,6 +39,19 @@ RSpec.describe QuestionsController, type: :controller do
 
     it 'renders new view' do
       expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET #edit' do
+
+    before { get :edit, params: {id: question} }
+
+    it 'assigns the edited question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders edit view' do
+      expect(response).to render_template :edit
     end
   end
 
